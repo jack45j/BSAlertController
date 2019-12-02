7d63bc4b2446397fed718b62c6901cced37a4459
Benson <ITOC59@chinalife.com.tw>
2019-12-02 09:30:41 +0800

    Initial Commit

---
+++ BSAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BSAlertController
+//
+//  Created by Chinalife on 2019/12/2.
+//  Copyright © 2019 YochaStudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
